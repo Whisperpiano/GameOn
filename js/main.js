632291c3331da9ac2cbd49c53d1df87deba4b467
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-01-25 09:52:39 +0100

    Slider Header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 console.log('Holaaaaa')
+
+const slider = document.querySelector('.slider')
+const sliderControls = document.querySelectorAll('#controls a')
+
+sliderControls.forEach((button,index)=>{
+    button.addEventListener('click',()=>{
+        document.querySelector('.selected').classList.remove('selected')
+        button.classList.add('selected')
+        slider.style.transform = `translateX(-${index*100}%)`
+    })
+})
