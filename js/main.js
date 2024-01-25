36fc587d1d7f0026c9350449f8739117b7bf58d4
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-01-25 10:14:38 +0100

    home page - reviews slider

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 console.log('Holaaaaa')
 
-const slider = document.querySelector('.slider')
+const mainSlider = document.querySelector('.slider')
+const reviewSlider = document.querySelector('#reviews-slider')
+
 const sliderControls = document.querySelectorAll('#controls a')
+const reviewSliderControls = document.querySelectorAll('#reviews-controls a')
 
 sliderControls.forEach((button,index)=>{
     button.addEventListener('click',()=>{
         document.querySelector('.selected').classList.remove('selected')
         button.classList.add('selected')
-        slider.style.transform = `translateX(-${index*100}%)`
+        mainSlider.style.transform = `translateX(-${index*100}%)`
+    })
+})
+
+reviewSliderControls.forEach((button,index)=>{
+    button.addEventListener('click', ()=> {
+        document.querySelector('.rev-selected').classList.remove('rev-selected')
+        button.classList.add('rev-selected')
+        reviewSlider.style.transform = `translateX(-${index*100}%)`
     })
 })
